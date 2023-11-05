ae5f43389e1d5006335f6e919395b078abff23fc
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-11-05 23:40:56 +0500

    feat: Added sample test formulae, in a loop

--- main.py
+++ main.py
@@ -16,16 +16,27 @@ def Horn(formula):
             left, right = match.groups()
             props = left[0].split('^')
 
-        if set(props).issubset(marked):
-            if right != 'F':
-                marked.add(right)
-            else:
-                return 0
+            if set(props).issubset(marked):
+                if right != 'F':
+                    marked.add(right)
+                else:
+                    return 0
             
     # Step 3 of algo, where we conclude, that the formula is satisfiable
     return 1
     
 if __name__ == '__main__':
-    formula = input("Enter a valid Horn Formula: ")
-    # formula = "(s->q)^(T->s)^(q^s->p)"
-    print(f"The formula is {'satisfiable' if Horn(formula) else 'not satisfiable'}")
+    # formula = input("Enter a valid Horn Formula: ")
+    # print(f"The formula is {'satisfiable' if Horn(formula) else 'not satisfiable'}")
+
+    formulae = [
+                '(p^q->r)^(p->T)',
+                '(p^q^s->p)^(q^r->p)^(p^s->s)', 
+                '(p->s)^(q^r->p)^(r^s->F)', 
+                '(T->q)^(T->s)^(q^r->p)^(p^q^s->F)^(T->r)', 
+                '(q->p)^(q^r^t->F)^(T->s)^(s->q)',
+                '(p->q)^(q->r)^(r->s)^(p^r->s)'
+                ]
+
+    for formula in formulae:
+        print(f"The formula {formula} is {'satisfiable' if Horn(formula) else 'not satisfiable'}")
